c6da9ff91b4138cd493532c2be355d5eec8d20d9
Admin <Admin@JIVIN-DIYA-PC>
2024-10-07 00:36:13 +0530

    Add project files.

---
+++ CustomAttribute/DeBugInfo.cs
@@ -0,0 +1,56 @@
+﻿namespace CustomAttribute
+{
+    [AttributeUsage(
+   AttributeTargets.Class |
+   AttributeTargets.Constructor |
+   AttributeTargets.Field |
+   AttributeTargets.Method |
+   AttributeTargets.Property,
+   AllowMultiple = true)]
+    internal class DeBugInfo : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
+
+        public DeBugInfo(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+        public int BugNo
+        {
+            get
+            {
+                return bugNo;
+            }
+        }
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+}
---
+++ CustomAttribute/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CustomAttribute;
+using System.Reflection;
+
+Rectangle r = new Rectangle(4.5, 7.5);
+r.Display();
+Type type = typeof(Rectangle);
+
+//iterating through the attributes of the Rectangle class
+foreach (Object attributes in type.GetCustomAttributes(false))
+{
+    DeBugInfo dbi = (DeBugInfo)attributes;
+
+    if (null != dbi)
+    {
+        Console.WriteLine("Bug no: {0}", dbi.BugNo);
+        Console.WriteLine("Developer: {0}", dbi.Developer);
+        Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+        Console.WriteLine("Remarks: {0}", dbi.Message);
+    }
+}
+
+//iterating through the method attributes
+foreach (MethodInfo m in type.GetMethods())
+{
+
+    foreach (Attribute a in m.GetCustomAttributes(true))
+    {
+        DeBugInfo dbi = (DeBugInfo)a;
+
+        if (null != dbi)
+        {
+            Console.WriteLine("Bug no: {0}, for Method: {1}", dbi.BugNo, m.Name);
+            Console.WriteLine("Developer: {0}", dbi.Developer);
+            Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+            Console.WriteLine("Remarks: {0}", dbi.Message);
+        }
+    }
+}
+Console.ReadLine();
+      
